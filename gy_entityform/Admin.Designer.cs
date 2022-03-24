e4baf6f571a9cb50f826e470fde8d9374e63ce8a
Batu YURT <asdasd019@hotmail.com>
2022-03-24 21:02:53 +0300

    .

--- gy_entityform/Admin.Designer.cs
+++ gy_entityform/Admin.Designer.cs
@@ -31,6 +31,8 @@ namespace gy_entityform
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,11 +55,31 @@ namespace gy_entityform
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(47, 166);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 59);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Product Panel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(47, 283);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(188, 59);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "User Panel";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Admin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Admin";
@@ -72,5 +94,7 @@ namespace gy_entityform
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
